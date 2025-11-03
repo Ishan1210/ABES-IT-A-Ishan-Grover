46e9c1a2a1bf79504379e585aa7291deb688c696
Ishan1210 <ishangrover121005@gmail.com>
2025-11-03 12:35:45 +0530

    button colour

--- netflix/src/components/Count1.jsx
+++ netflix/src/components/Count1.jsx
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 
 function Count1() {
   const [count, setCount] = useState(0);
+  const [btnColor, setBtnColor] = useState('red');
 
-  function increment() {
-    setCount(count + 1);
-  }
-
-  function decrement() {
-    setCount(count - 1);
+  function changeColor() {
+    setBtnColor(btnColor === 'red' ? 'green' : 'red');
   }
 
   return (
     <div>
-      <button onClick={increment}>+</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
       <span>{count}</span>
-      <button onClick={decrement}>-</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <br />
+      <button onClick={changeColor} style={{ backgroundColor: btnColor, color: 'white' }}>
+        {btnColor === 'red' ? 'Red' : 'Green'}
+      </button>
     </div>
   );
 }
