34d0b3678e167150977a6fa4a70637b3efcdbc90
Ishan1210 <ishangrover121005@gmail.com>
2025-11-03 12:57:29 +0530

    book app

---
+++ Book_App/my-react-app/src/components/Book.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './book.css'
+function book() {
+  return (
+    <div id='book'>
+      <img src="" alt="" height={100} width={100}/>
+      <h1>Title:Math</h1>
+      <h1>250</h1>
+    </div>
+  )
+}
+
+export default book
