78b62867b13ac9749bf9fb65de2d025147c7b993
Ishan1210 <ishangrover121005@gmail.com>
2025-11-10 12:39:05 +0530

    link1

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Link1 from './components/link1';
+
+function App() {
+  return (
+    <div className="App">
+      <Link1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/components/link1.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Routes,Route,Link} from 'react-router-dom'
+function Home(){
+    return <h1>This my Home Page</h1>
+}
+
+function link1() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default link1
